bf51d4da9afdd0fe09a42afa002ea6165e120cf6
kshreeya2006 <kshreeya2006@gmail.com>
2025-06-06 14:35:20 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return <footer>&copy; 2005. All rights Reserved.</footer>;
+  return <footer>&copy; 2025. All rights Reserved.</footer>;
 }
